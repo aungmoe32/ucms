36220522b4a6bf381d27d4ae689fe6f9aa263451
Aung Moe Myint Thu <kokopu.zawgyi01@gmail.com>
2024-07-19 00:18:23 +0630

    support series edit of recurring events

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
 import Table from "@/components/Table";
-import { getEvents } from "@/lib/calendar";
+import { formatDateTime, getEvents } from "@/lib/calendar";
+import { calendar_v3 } from "googleapis";
 
 export default async function Home() {
   const items = await getEvents();
   // console.log(items);
+
+  // const filteredEvents = items.map((event): calendar_v3.Schema$Event => {
+  //   const recurrId = event.recurringEventId;
+  //   // if exception instance
+  //   if (recurrId) {
+  //     const recurrEvent = items.find((e) => e.id == recurrId);
+  //     const exDate = recurrEvent!.exDate;
+  //     recurrEvent!.exDate = exDate
+  //       ? exDate +
+  //         "," +
+  //         formatDateTime(new Date(event.originalStartTime!.dateTime!))
+  //       : formatDateTime(new Date(event.originalStartTime!.dateTime!));
+  //   }
+  //   return event;
+  // });
+
+  // console.log(formatDateTime(new Date("2024-07-16T10:15:00+06:30")));
   return <Table events={items}></Table>;
 }
--- components/Table.tsx
+++ components/Table.tsx
@@ -5,7 +5,7 @@ import { calendar_v3 } from "googleapis";
 import { Scheduler, View, Editing } from "devextreme-react/scheduler";
 import { appointments } from "../lib/data";
 import { useCallback, useState } from "react";
-import { getEvents } from "@/lib/calendar";
+import { formatDateTime, getEvents } from "@/lib/calendar";
 import CustomStore from "devextreme/data/custom_store";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -13,11 +13,15 @@ const TimeZone = "Asia/Yangon";
 
 function fixRruleStr(event: any, remove: boolean) {
   let recurr = event.recurrence;
+  //   console.log(recurr);
   if (!recurr) return;
   if (!Array.isArray(recurr)) {
     recurr = Object.values(recurr);
   }
-  //   console.log(recurr);
+  if (recurr[0] == "") {
+    event.recurrence = [];
+    return;
+  }
   if (recurr?.length) {
     event.recurrence = [
       remove ? recurr[0].replace("RRULE:", "") : `RRULE:${recurr[0]}`,
@@ -29,12 +33,12 @@ function Table({ events }: { events: calendar_v3.Schema$Event[] }) {
   const router = useRouter();
   const dataSource = new CustomStore({
     load: async (options) => {
-      //   console.log("load ", options);
+      // console.log("load ", options);
       const mEvents = events.map((event: calendar_v3.Schema$Event) => {
         fixRruleStr(event, true);
         return event;
       });
-      //   console.log(mEvents);
+      console.log(mEvents);
       return mEvents;
     },
 
@@ -46,7 +50,19 @@ function Table({ events }: { events: calendar_v3.Schema$Event[] }) {
     },
     update: async (key, values) => {
       console.log(key, values);
+      //   console.log(values.recurrence);
+      // single edit
+      //   if (key.exDate && values.exDate && key.exDate != values.exDate) {
+      //     values.recurrence = undefined;
+      //     values.recurringEventId = values.id;
+      //     values.originalStartTime = {
+      //       dateTime: "key.exDate",
+      //       timeZone: TimeZone,
+      //     };
+      //     return;
+      //   }
 
+      fixRruleStr(values, false);
       const event = await axios.post(`/api/events/patch/${key.id}`, values);
       router.refresh();
     },
@@ -69,31 +85,56 @@ function Table({ events }: { events: calendar_v3.Schema$Event[] }) {
 
   return (
     <div className="App">
+      <RefreshBtn></RefreshBtn>
       <Scheduler
         id="scheduler"
         dataSource={dataSource}
+        // startDateExpr="startDate"
+        // endDateExpr="endDate"
         startDateExpr="start.dateTime"
         endDateExpr="end.dateTime"
-        startDateTimeZoneExpr="start.timeZone"
-        endDateTimeZoneExpr="end.timeZone"
+        // startDateTimeZoneExpr="start.timeZone"
+        // endDateTimeZoneExpr="end.timeZone"
         textExpr="summary"
         // textExpr="title"
         recurrenceRuleExpr="recurrence[0]"
+        recurrenceExceptionExpr="exDate"
         defaultCurrentDate={currentDate}
         // currentDate={currentDate}
-        onOptionChanged={handlePropertyChange}
+        // onOptionChanged={handlePropertyChange}
         // remoteFiltering={true}
         defaultCurrentView="week"
         timeZone={TimeZone}
         adaptivityEnabled={true}
+        recurrenceEditMode="series"
+        // onAppointmentAdding={(d) => {
+        //   console.log("adding", d);
+        // }}
       >
         <View type="day" startDayHour={10} endDayHour={22} />
-        <View type="week" startDayHour={10} endDayHour={22} />
+        <View type="week" startDayHour={6} endDayHour={22} />
         <View type="month" />
-        {/* <Editing allowTimeZoneEditing={true} allowDragging={false} /> */}
+        <Editing allowDragging={true} />
       </Scheduler>
     </div>
   );
 }
 
 export default Table;
+
+const RefreshBtn = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <button
+        className=" hover:text-blue-400 "
+        onClick={() => {
+          router.refresh();
+          // toast.success("refreshed");
+        }}
+      >
+        Refresh
+      </button>
+    </div>
+  );
+};
