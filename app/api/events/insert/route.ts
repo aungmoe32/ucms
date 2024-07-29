ddfb1fff8d4c46bcb5af9c5068328cd23c14fbf0
Aung Moe Myint Thu <kokopu.zawgyi01@gmail.com>
2024-07-29 11:07:02 +0630

    initial project

--- app/api/events/insert/route.ts
+++
@@ -1,9 +0,0 @@
-import { insertEvent } from "@/lib/calendar";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(request: NextRequest) {
-  const data = await request.json();
-  //   console.log(data);
-  const event = await insertEvent(data);
-  return NextResponse.json(event);
-}
