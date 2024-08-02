c178eca94a80cc1cacafa1ae3b41a14afa653e39
Aung Moe Myint Thu <kokopu.zawgyi01@gmail.com>
2024-08-02 14:06:03 +0630

    initial drizzle without nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import NavBar from "@/components/NavBar";
-import { db } from "@/lib/drizzle/db";
-import { users } from "@/lib/drizzle/schema";
-import { signIn, signOut } from "next-auth/react";
-import Link from "next/link";
-import React from "react";
-
 const page = async () => {
   // await db.insert(users).values({
   //   name : "hacker",
   //   email : "pogoka9717@maxturns.com",
   // })
-  return (
-    <div>
-      <NavBar></NavBar>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
--- lib/drizzle/db.ts
+++ lib/drizzle/db.ts
@@ -1,10 +1,8 @@
-import 'dotenv/config';
-
 // import { neon } from '@neondatabase/serverless';
 // import { drizzle } from 'drizzle-orm/neon-http';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
 // const client = neon(process.env.DATABASE_URL!);
 const client = postgres(process.env.DATABASE_URL!);
