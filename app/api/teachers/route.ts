234cc5d8932505ff0c52425571ea7806fffb01a6
aungmoe32 <aungmoemyintthu@gmail.com>
2024-08-09 14:18:05 +0630

    bug of defer constraint not work in transaction with neon

--- app/api/teachers/route.ts
+++ app/api/teachers/route.ts
@@ -21,47 +21,53 @@ export async function POST(request: NextRequest) {
     if (!validation.success)
       return NextResponse.json(validation.error.format(), { status: 400 });
 
-    const teacher = await db.transaction(async (tx) => {
-      let salt = genSaltSync(10);
-      let hash = hashSync(body.password, salt);
-      const user = await tx
-        .insert(users)
-        .values({
-          name: body.name,
-          email: body.email,
-          password: hash,
-          role: "teacher",
-          major: body.major,
-          gender: body.gender,
-        })
-        .returning({
-          id: users.id,
-        });
-      const teacher = await tx
-        .insert(teachers)
-        .values({
-          experience: 1,
-          userId: user[0].id,
-        })
-        .returning({
-          id: teachers.id,
-        });
+    const teacher = await db.transaction(
+      async (tx) => {
+        let salt = genSaltSync(10);
+        let hash = hashSync(body.password, salt);
+        const user = await tx
+          .insert(users)
+          .values({
+            name: body.name,
+            email: body.email,
+            password: hash,
+            role: "teacher",
+            major: body.major,
+            gender: body.gender,
+          })
+          .returning({
+            id: users.id,
+          });
+        const teacher = await tx
+          .insert(teachers)
+          .values({
+            experience: 1,
+            userId: user[0].id,
+          })
+          .returning({
+            id: teachers.id,
+          });
 
-      let datas: {
-        teacher_id: string;
-        semester_id: string;
-      }[] = [];
-      body.semesters.forEach((semesterId) => {
-        datas.push({
-          teacher_id: teacher[0].id,
-          semester_id: semesterId,
+        let datas: {
+          teacher_id: string;
+          semester_id: string;
+        }[] = [];
+        body.semesters.forEach((semesterId) => {
+          datas.push({
+            teacher_id: teacher[0].id,
+            semester_id: semesterId,
+          });
         });
-      });
-      //   console.log(datas);
-      await db.insert(teacher_semester).values(datas);
+        //   console.log(datas);
+        await db.insert(teacher_semester).values(datas);
 
-      return teacher;
-    });
+        return teacher;
+      },
+      {
+        isolationLevel: "read committed",
+        deferrable: true,
+      }
+    );
 
     return NextResponse.json(teacher, { status: 201 });
   } catch (e) {
